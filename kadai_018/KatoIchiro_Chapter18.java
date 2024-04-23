c1f365f6406d425c1fb3db7ea3c06acedf2f2d94
samurai-koi306 <167387253+samurai-koi306@users.noreply.github.com>
2024-04-23 11:46:23 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+public class KatoIchiro_Chapter18  extends Kato_Chapter18{
+	public void setGivenName() {
+		String givenName=("一郎");
+		
+	}
+	public void eachIntroduce() {
+	System.out.println("好きな食べ物はリンゴです");
+	
+}
+	void givenName(String givenName) {
+		this.givenName=givenName;
+		
+	}
+
+}
