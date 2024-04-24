933d1c849e7295fc3a3d38e04c6e97d096989a2e
samurai-koi306 <167387253+samurai-koi306@users.noreply.github.com>
2024-04-24 15:53:51 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,32 @@
+package kadai_018;
+public class KatoExec_Chapter18{
+	 public static void main(String[] args) {
+		 
+		 KatoTaro_Chapter18 taro=new KatoTaro_Chapter18();
+		 taro.givenName="太郎";
+		 KatoIchiro_Chapter18 ichiro=new KatoIchiro_Chapter18();
+		 ichiro.givenName="一郎";
+		 KatoHanako_Chapter18 hanako=new KatoHanako_Chapter18();
+		 hanako.givenName="花子";
+
+
+		 
+		 
+		 taro.setGivenName();
+		 taro.execIntroduce();
+		 System.out.println();
+		 ichiro.setGivenName();
+		 ichiro.execIntroduce();
+		 System.out.println();
+		 hanako.setGivenName();
+		 hanako.execIntroduce();
+		 System.out.println();
+		 
+		 
+		 
+	 }
+	
+	
+	
+	
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	public void setGivenName() {
+		String givenName=("太郎");
+		 
+	}
+	public void eachIntroduce() {
+		System.out.println("Javaが得意です");
+		
+	}
+	void givenName(String givenName) {
+		this.givenName=givenName;
+	}
+
+}
